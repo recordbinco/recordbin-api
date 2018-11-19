42c89c3f0bec69d4f8ba6b6ecb92326bf9bd7bcc
gtalarico <gtalarico@gmail.com>
2018-11-19 13:52:11 -0500

    Flake8 formatting

--- backend/tableau_connector.py
+++ backend/tableau_connector.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from django.views.generic import TemplateView
 from django.views.decorators.cache import never_cache
 from django.http import JsonResponse
